fbad717d38a0221fbd9b648dae8a05d75cb2a143
SpencerWie <spdwiecz@iupui.edu>
2016-10-04 22:24:04 -0400

    Adding MvcMoive

---
+++ MvcMovie/src/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name = "User", int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, numTimes: {numTimes}");
+        }
+    }
+}
