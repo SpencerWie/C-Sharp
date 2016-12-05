9afc2db97d692be617eedfa9fa9e49ad752b267c
SpencerWie <spdwiecz@iupui.edu>
2016-12-04 21:59:15 -0500

    Added functionality to load a map from JSON data (from Tiled app)

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Game1
 {
@@ -12,13 +14,15 @@ namespace Game1
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Color bgColor = Color.White;
+        Color bgColor = new Color(25, 23, 22);
 
         Player player;
 
         Texture2D background;
 
         SpriteFont font;
+
+        int[] mapArray;
 
         public Game1()
         {
@@ -53,9 +57,11 @@ namespace Game1
             int windowHeight = GraphicsDevice.Viewport.Bounds.Height / 2;
             player.position = new Vector2(windowWidth - (player.spriteSize/2), windowHeight - (player.spriteSize / 2)); // Move player to center
 
-            background = Content.Load<Texture2D>(@"background");
+            background = Content.Load<Texture2D>(@"cave");
             Scroller.X = 0;
             Scroller.Y = 0;
+
+            LoadJson("cave_1.json");
 
             font = Content.Load<SpriteFont>("Arial");
         }
@@ -90,17 +96,41 @@ namespace Game1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(bgColor);
 
             // Draw while not interpleting (bluring) half-pixels 
             spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointWrap, null, null, null, Matrix.CreateTranslation(-Scroller.X, -Scroller.Y, 0));
 
             spriteBatch.Draw(background, new Rectangle(0, 0, background.Width, background.Height), Color.White);
-            spriteBatch.DrawString(font, "Frame Ticks: "+player.frameTicks, new Vector2(Scroller.X, Scroller.Y), Color.White);
+            drawMap(mapArray, background, 16, 32, spriteBatch);
             player.Draw(spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        public void LoadJson(string file)
+        {
+            string json;
+            using (StreamReader r = new StreamReader(file)) {
+                 json = r.ReadToEnd();
+            }
+            dynamic o = JsonConvert.DeserializeObject(json);
+            mapArray = (int[])o.layers[0].data.ToObject(typeof(int[]));
+        }
+
+        public void drawMap(int[] map, Texture2D tilemap, int tileSize, int drawSize ,SpriteBatch spriteBatch)
+        {
+            int tileWidth = 30;// Used to turn id's to positions 
+
+            for (int i = 0; i < map.Length; i++)
+            {
+                int x = (i % tileWidth) * drawSize;
+                int y = (int)(i / tileWidth) * drawSize;
+                int sourceX = ((map[i]-1) % tileSize) * tileSize;
+                int sourceY = ((map[i]-1) / tileSize) * tileSize;
+                spriteBatch.Draw(tilemap, new Rectangle(x, y, drawSize, drawSize), new Rectangle(sourceX, sourceY, tileSize, tileSize), Color.White);
+            }
         }
     }
 }
